655496385d91c430eddd41e04b569a2cab6ffb12
matias-e-berta <matiaseberta@gmail.com>
2022-06-20 16:44:09 -0300

    Added message if profit is zero, to buy at lowest price and wait

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,5 +1,5 @@
 def stock_picker stocks
-    return "Not enough info" if stocks.count<2
+    return puts "Not enough info" if stocks.count<2
     min = stocks[0]
     max = 0
     profit = 0
@@ -13,13 +13,22 @@ def stock_picker stocks
         end
         
     end
-    min = max-profit
-    puts "Best price to buy is #{min}, at day #{stocks.index(min)+1}" 
-    puts "Best price to sell is #{max}, at day #{stocks.index(max)+1}"
-    puts "Profit is #{max-min}"
+
+    if profit==0
+        puts "It's not worth to sell. Keep your stocks and wait for a rise on prices"
+        puts "Best price to buy is #{stocks.min}, at day #{stocks.index(stocks.min)+1}"
+
+    else
+        min = max-profit
+        puts "Best price to buy is #{min}, at day #{stocks.index(min)+1}" 
+        puts "Best price to sell is #{max}, at day #{stocks.index(max)+1}"
+        puts "Profit is #{max-min}"
+    end
+    
 end
 
-    stock_list = [5,9,11,3,18,6,17,2]
+    # stock_list = [5,9,11,3,8,6,17,2]
+    stock_list = [11,5,4,3]
 
     stock_picker stock_list
 
